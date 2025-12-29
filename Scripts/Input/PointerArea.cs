cbd38ccd903194a04e4d06e9d59b3e94e577c671
Yevhenii Kostenko <81772181+YevheniiKost@users.noreply.github.com>
2025-12-30 00:16:20 +0200

    Add pointer area

---
+++ Scripts/Input/PointerArea.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace YeKostenko.CoreKit.Input
+{
+    public class PointerArea : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        [SerializeField]
+        private UnityEvent _onPointerDownEvent;
+        [SerializeField]
+        private UnityEvent _onPointerUpEvent;
+        
+        public bool IsPointerDown { get; private set; }
+        
+        public event Action OnPointerDownEvent;
+        public event Action OnPointerUpEvent;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            IsPointerDown = true;
+            OnPointerDownEvent?.Invoke();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            IsPointerDown = false;
+            OnPointerUpEvent?.Invoke();
+        }
+    }
+}
