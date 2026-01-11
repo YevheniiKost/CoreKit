1e803a817a0c23fe3b0c44410790865f59ed9578
Yevhenii Kostenko <81772181+YevheniiKost@users.noreply.github.com>
2026-01-11 18:11:09 +0200

    Add common Command and Query interfaces

---
+++ Scripts/Utils/IQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace YevheniiKostenko.CoreKit.Utils
+{
+    public interface IQuery<out TResult>
+    {
+        TResult Execute();
+    }
+    
+    public interface IQuery<out TResult, in T>
+    {
+        TResult Execute(T parameter);
+    }
+}
