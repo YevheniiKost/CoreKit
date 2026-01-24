ac69a3ccfd586116fc07bfb017f17614c3924db0
YevheniiKost <atom903@gmail.com>
2026-01-24 17:18:45 +0200

    Add factory util interface

---
+++ Scripts/Utils/IFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace YevheniiKostenko.CoreKit.Utils
+{
+    public interface IFactory<out T>
+    {
+        T Create();
+    }
+
+    public interface IFactory<out TResult, in TParam>
+    {
+        TResult Create(TParam param);
+    }
+}
