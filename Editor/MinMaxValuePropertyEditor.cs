98f8a9f033a05080f62da3a71c5b7dc2f5add98c
Yevhenii Kostenko <81772181+YevheniiKost@users.noreply.github.com>
2026-03-22 08:51:20 +0200

    Add MinMaxValue

---
+++ Editor/MinMaxValuePropertyEditor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using YeKostenko.CoreKit.Structs;
+
+namespace YeKostenko.CoreKit.Editor
+{
+    [CustomPropertyDrawer(typeof(MinMaxValue))]
+    public class MinMaxValuePropertyEditor : PropertyDrawer
+    {
+        private const float LabelWidth = 30f;
+        private const float Spacing = 5f;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw label
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            // Don't make child fields be indented
+            int indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            // Calculate rects
+            float fieldWidth = (position.width - (LabelWidth * 2) - Spacing) / 2;
+            Rect minLabelRect = new Rect(position.x, position.y, LabelWidth, position.height);
+            Rect minFieldRect = new Rect(position.x + LabelWidth, position.y, fieldWidth, position.height);
+            Rect maxLabelRect = new Rect(position.x + LabelWidth + minFieldRect.width + Spacing,
+                position.y, LabelWidth, position.height);
+            Rect maxFieldRect = new Rect(position.x + (LabelWidth * 2) + minFieldRect.width + Spacing,
+                position.y, minFieldRect.width, position.height);
+
+            // Find the properties
+            SerializedProperty minProp = property.FindPropertyRelative("_min");
+            SerializedProperty maxProp = property.FindPropertyRelative("_max");
+
+            // Draw fields
+            EditorGUI.LabelField(minLabelRect, "Min");
+            EditorGUI.PropertyField(minFieldRect, minProp, GUIContent.none);
+            EditorGUI.LabelField(maxLabelRect, "Max");
+            EditorGUI.PropertyField(maxFieldRect, maxProp, GUIContent.none);
+
+            // Ensure Min is not greater than Max
+            if (minProp.floatValue > maxProp.floatValue)
+            {
+                minProp.floatValue = maxProp.floatValue;
+            }
+
+            // Set indent back to what it was
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
---
+++ Scripts/Structs/MinMaxValue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace YeKostenko.CoreKit.Structs
+{
+    [Serializable]
+    public class MinMaxValue : IEquatable<MinMaxValue>, IFormattable
+    {
+        [SerializeField]
+        private float _min;
+        [SerializeField]
+        private float _max;
+
+        public MinMaxValue(float min, float max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public MinMaxValue()
+        {
+            Min = 0;
+            Max = 0;
+        }
+
+        public float Min
+        {
+            get => _min;
+            set => _min = Mathf.Min(value, Max);
+        }
+
+        public float Max
+        {
+            get => _max;
+            set => _max = Mathf.Max(value, Min);
+        }
+
+        public float GetRandomValue()
+        {
+            return UnityEngine.Random.Range(Min, Max);
+        }
+
+        public bool Equals(MinMaxValue other) => Min.Equals(other.Min) && Max.Equals(other.Max);
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return $"Min: {Min.ToString(format, formatProvider)}, Max: {Max.ToString(format, formatProvider)}";
+        }
+    }
+}
